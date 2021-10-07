884bb9a630b22ab7c94f2724aa707fa2edf1ae60
Daniel Callus <danielcallus@gmail.com>
2021-10-07 16:58:29 +0100

    stripped more

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,15 @@ import React from 'react';
 import './App.css';
 import UiContainer from './containers/UiContainer'
 import Header from './components/Header'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
 
 
 const App = () => {
    
     return (
-      <Router>
         <div className="App">
           <Header/>
             <UiContainer />
         </div>
-      </Router>
     );
   
 }
