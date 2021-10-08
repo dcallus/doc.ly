6c3c3f5d037de074e4efbfeb033065a89e0cab13
Daniel Callus <danielcallus@gmail.com>
2021-10-08 13:34:15 +0100

    test data

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,3 +1,6 @@
 use doctors;
 db.dropDatabase();
-db.appointments.insert({});
+db.appointments.insert({doctor: "My Local Doctor",
+                        first_name: "Dan",
+                        last_name: "Callus",
+                        date_time: 1633696276});
