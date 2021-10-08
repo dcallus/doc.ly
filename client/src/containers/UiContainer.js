e34e1e09376743f1c0937a73140e1768f5dc60dd
Daniel Callus <danielcallus@gmail.com>
2021-10-08 13:38:51 +0100

    minor refactor

--- client/src/containers/UiContainer.js
+++ client/src/containers/UiContainer.js
@@ -1,7 +1,6 @@
 import React, {} from "react";
 import Map from "./Map";
 import Footer from "../components/Footer";
-import NavBarBottom from "../components/NavBarBottom";
 
 const UiContainer = () => {
 
@@ -9,7 +8,6 @@ const UiContainer = () => {
   return (
     <div id="Ui-Container">
       <Map />
-      <NavBarBottom />
       <Footer />
     </div>
   )
